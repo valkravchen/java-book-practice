8c6d9dc664a2283d78127b3687d2bcdce9bb6ca6
Valk <kravchenkoval@mail.ru>
2025-12-16 11:59:36 +0300

    Add method printlnWithLambda
    Пример 1.8: Использование ссылки на метод для доступа к println

--- src/main/java/com/kousen/modernjavarecipes/chapter01/MethodReferences.java
+++ src/main/java/com/kousen/modernjavarecipes/chapter01/MethodReferences.java
@@ -1,4 +1,15 @@
 package com.kousen.modernjavarecipes.chapter01;
 
+import java.util.stream.Stream;
+
 public class MethodReferences {
+    public static void printlnWithLambda() {
+        Stream.of(3, 1, 4, 1, 5, 9)
+                .forEach(x -> System.out.println(x));
+
+    }
+
+    static void main() {
+        printlnWithLambda();
+    }
 }
