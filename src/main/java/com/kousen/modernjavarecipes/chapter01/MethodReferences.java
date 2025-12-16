e8b8ca6a540ae420580447670853320a82d1528c
Valk <kravchenkoval@mail.ru>
2025-12-16 12:55:54 +0300

    Add method printlnWithMethodReference
    Пример 1.8: Использование ссылки на метод для доступа к println

--- src/main/java/com/kousen/modernjavarecipes/chapter01/MethodReferences.java
+++ src/main/java/com/kousen/modernjavarecipes/chapter01/MethodReferences.java
@@ -4,11 +4,18 @@ import java.util.stream.Stream;
 
 public class MethodReferences {
     public static void printlnWithLambda() {
-        Stream.of(3, 1, 4, 1, 5, 9).forEach(x -> System.out.println(x));
+        Stream.of(3, 1, 4, 1, 5, 9)
+                .forEach(x -> System.out.println(x));
 
+    }
+
+   public static void printlnWithMethodReference() {
+        Stream.of(3, 1, 4, 1, 5, 9)
+                .forEach(System.out::println);
     }
 
     static void main() {
         printlnWithLambda();
+        printlnWithMethodReference();
     }
 }
