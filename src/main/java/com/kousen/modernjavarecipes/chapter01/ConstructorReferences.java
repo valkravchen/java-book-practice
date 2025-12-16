aa0f93e6e8e11ce813469fac6b92923fc87d87f2
Valk <kravchenkoval@mail.ru>
2025-12-16 16:45:06 +0300

    Add method convertPersonsToNamesWithMethodReference
    Пример 1.13: Преобразование списка людей в список имен

--- src/main/java/com/kousen/modernjavarecipes/chapter01/ConstructorReferences.java
+++ src/main/java/com/kousen/modernjavarecipes/chapter01/ConstructorReferences.java
@@ -1,4 +1,33 @@
 package com.kousen.modernjavarecipes.chapter01;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ConstructorReferences {
+    public static void convertPersonsToNamesWithLambda() {
+        List<Person> people = Arrays.asList(
+                new Person("Grace Hopper"),
+                new Person("Barbara Liskov"),
+                new Person("Ada Lovelace")
+        );
+        List<String> names = people.stream()
+                .map(person -> person.getName())
+                .toList();
+    }
+
+    public static void convertPersonsToNamesWithMethodReference(){
+        List<Person> people = Arrays.asList(
+                new Person("Grace Hopper"),
+                new Person("Barbara Liskov"),
+                new Person("Ada Lovelace")
+        );
+        List<String> names = people.stream()
+                .map(Person::getName)
+                .toList();
+    }
+
+    static void main() {
+        convertPersonsToNamesWithLambda();
+        convertPersonsToNamesWithMethodReference();
+    }
 }
