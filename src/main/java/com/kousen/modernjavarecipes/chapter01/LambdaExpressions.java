8e66b5678fd9787b2c3ef4dcd143074e7702715b
Valk <kravchenkoval@mail.ru>
2025-12-16 11:15:46 +0300

    Add method filenameFilterWithAnonymousClass
    Пример 1.4: Реализация FilenameFilter с помощью анонимного внутреннего класса

--- src/main/java/com/kousen/modernjavarecipes/chapter01/LambdaExpressions.java
+++ src/main/java/com/kousen/modernjavarecipes/chapter01/LambdaExpressions.java
@@ -1,5 +1,9 @@
 package com.kousen.modernjavarecipes.chapter01;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+
 public class LambdaExpressions {
 
     public static void runnableWithAnonymousClass() {
@@ -12,11 +16,29 @@ public class LambdaExpressions {
     }
 
     public static void runnableWithLambda() {
-        new Thread(()-> System.out.println("внутри конструктора Thread с использованием лямбды")).start();
+        new Thread(() -> System.out.println("внутри конструктора Thread с использованием лямбды")).start();
+    }
+
+    public static void runnableAsVariable() {
+        Runnable runnable = () -> System.out.println("лямбда-выражение, реализующее метод run");
+        new Thread(runnable).start();
+    }
+
+    public static void filenameFilterWithAnonymousClass() {
+        File directory = new File("./src/main/java");
+        String[] names = directory.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".java");
+            }
+        });
+        System.out.println(Arrays.asList(names));
     }
 
     static void main() {
         runnableWithAnonymousClass();
         runnableWithLambda();
+        runnableAsVariable();
+        filenameFilterWithAnonymousClass();
     }
 }
