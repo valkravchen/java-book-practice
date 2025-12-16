089a134b7cde435e8f0b141819f993c79709d74b
Valk <kravchenkoval@mail.ru>
2025-12-16 16:30:24 +0300

    Add class Person

---
+++ src/main/java/com/kousen/modernjavarecipes/chapter01/Person.java
@@ -0,0 +1,42 @@
+package com.kousen.modernjavarecipes.chapter01;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
